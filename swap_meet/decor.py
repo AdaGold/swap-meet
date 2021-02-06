41082456be7b2068b30947282ce260e06959f05f
Jared <jared@adadevelopersacademy.org>
2021-02-05 16:50:58 -0800

    remove solution

--- swap_meet/decor.py
+++
@@ -1,9 +0,0 @@
-from swap_meet.item import Item
-
-class Decor(Item):
-
-  def __init__(self, condition=0):
-    super().__init__("Decor", condition)
-
-  def __str__(self):
-    return "Something to decorate your space."
