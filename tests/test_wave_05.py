c8a901bfd59e459b750dc3ad2d0e891a9c513125
Jared <jared@adadevelopersacademy.org>
2021-02-08 14:19:36 -0800

    Add tests for condition_description

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -33,4 +33,21 @@ def test_items_have_condition_as_float():
     for item in items:
         assert item.condition == pytest.approx(3.5)
 
+def test_items_have_condition_descriptions_that_are_the_same_regardless_of_type():
+    items = [
+        Clothing(condition=5),
+        Decor(condition=5),
+        Electronics(condition=5)
+    ]
+    five_condition_description = items[0].condition_description()
+    for item in items:
+        assert item.condition_description() == five_condition_description
+
+    item[0].condition = 1
+    one_condition_description = items[0].condition_description()
+    for item in items:
+        item.condition = 1
+        assert item.condition_description() == one_condition_description
+
+    assert one_condition_description != five_condition_description
 
