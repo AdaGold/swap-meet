b18c45a9f637201464ceae4e4821f10c46012e24
Chris McAnally <chris@adadevelopersacademy.org>
2021-03-30 16:06:19 -0700

    tests mod

---
+++ test_vendor.py
@@ -0,0 +1,19 @@
+from swap_meet.vendor import Vendor
+from swap_meet.clothing import Clothing
+from swap_meet.electronics import Electronics
+from swap_meet.decor import Decor
+
+
+item_a = Clothing(condition=2.0)
+item_b = Decor(condition=2.0)
+item_c = Clothing(condition=4.0)
+item_d = Decor(condition=5.0)
+item_e = Clothing(condition=3.0)
+tai = Vendor(
+    inventory=[item_a, item_b, item_c, item_d, item_e]
+)
+
+best_item = tai.get_best_by_category("Clothing")
+
+print(best_item.category)
+print(best_item.condition)
