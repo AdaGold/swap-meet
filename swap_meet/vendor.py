db9cb0a4a46c67924d9f661b4728e4efffc3f413
Lia Gaetano <lgaetano19@gmail.com>
2021-09-27 05:00:23 -0700

    Created Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    ''' '''
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, inventory_item):
+        ''' Add an item to instance inventory list. Returns
+        item.'''
+        self.inventory.append(inventory_item)
+        return inventory_item
+
+    def remove(self, inventory_item):
+        ''' Removes item from list, returning item. Returns
+        False if not found.'''
+        try:
+            self.inventory.remove(inventory_item)
+            return inventory_item
+        except ValueError:
+            return False
