b0860eed8b7af7831b3275c15d9eebf1de4138ae
Lia Gaetano <lgaetano19@gmail.com>
2021-09-27 22:45:59 -0700

    Create Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,23 @@
 class Decor:
-    pass
+    """
+    A class to represent an item of decor traded at the swap meet.
+    
+    ...
+
+    Attributes
+    ----------
+    category : str
+        categorical descriptor for decor
+
+    Methods
+    ------- 
+    __str__():
+        Method to stringify Decor. Returns default message.
+    """
+
+    def __init__(self, category="Decor"):
+        self.category = category
+
+    def __str__(self):
+        """Method to stringify Decor. Returns default message."""
+        return "Something to decorate your space."
