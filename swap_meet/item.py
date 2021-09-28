030c0bb607a856cbad3198f5d8e8a12b8e272b2f
Lia Gaetano <lgaetano19@gmail.com>
2021-09-28 08:51:21 -0700

    Add condition description

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -13,6 +13,9 @@ class Item:
     ------- 
     __str__():
         Method to stringify Item. Returns default 'hello world!' message.
+    
+    condition_description(self):
+        Method that describes Item based on it's condition rating. 
     """
 
     def __init__(self, category="", condition=0):
@@ -22,4 +25,17 @@ class Item:
     def __str__(self):
         """Method to stringify Item. Returns default message."""
         return "Hello World!"
+
+    def condition_description(self):
+        """Method that describes Item based on it's condition rating."""
+        if self.condition > 4:
+            return "Like new!"
+        elif self.condition > 3:
+            return "In good shape!"
+        elif self.condition > 2:
+            return "Fair..."
+        elif self.condition > 1:
+            return "Passable, but not great."
+        else:
+            return "Poor Quality"
 
