f2e585256a4e377611b20bc16cf3dbf76d904bb9
Lia Gaetano <lgaetano19@gmail.com>
2021-09-27 22:25:25 -0700

    Added Item docstring

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,24 @@
 class Item:
-    ''' '''
+    """
+    A class to represent an item traded at the swap meet.
+    
+    ...
+
+    Attributes
+    ----------
+    category : str
+        categorical descriptor for item
+
+    Methods
+    ------- 
+    __str__():
+        Method to stringify Item. Returns default 'hello world!' message.
+    """
 
     def __init__(self, category=""):
         self.category = category
 
     def __str__(self):
+        """Method to stringify Item. Returns default message."""
         return "Hello World!"
 
