116d5309f483c7cc101a32743b397fe7999387e9
Lia Gaetano <lgaetano19@gmail.com>
2021-09-27 22:48:25 -0700

    Create Electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,23 @@
 class Electronics:
-    pass
+    """
+    A class to represent an item of electronics traded at the swap meet.
+    
+    ...
+
+    Attributes
+    ----------
+    category : str
+        categorical descriptor for electronics
+
+    Methods
+    ------- 
+    __str__():
+        Method to stringify Electronics. Returns default message.
+    """
+
+    def __init__(self, category="Electronics"):
+        self.category = category
+
+    def __str__(self):
+        """Method to stringify Electronics. Returns default message."""
+        return "A gadget full of buttons and secrets."
