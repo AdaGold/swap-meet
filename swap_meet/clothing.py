d31a6b3251d7de0fdac7d20bb3fba4d5e6f50dd6
Lia Gaetano <lgaetano19@gmail.com>
2021-09-27 22:44:10 -0700

    Create Clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,23 @@
 class Clothing:
-    pass
+    """
+    A class to represent an item of clothing traded at the swap meet.
+    
+    ...
+
+    Attributes
+    ----------
+    category : str
+        categorical descriptor for clothing
+
+    Methods
+    ------- 
+    __str__():
+        Method to stringify Clothing. Returns default message.
+    """
+
+    def __init__(self, category="Clothing"):
+        self.category = category
+
+    def __str__(self):
+        """Method to stringify Clothing. Returns default message."""
+        return "The finest clothing you could wear."
