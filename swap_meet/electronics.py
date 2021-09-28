5ea4905da571fc8b32da6893109b92e5b2635b78
Lia Gaetano <lgaetano19@gmail.com>
2021-09-28 09:08:50 -0700

    Update init to include condition and inheritance from Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,6 @@
-class Electronics:
+from .item import Item
+
+class Electronics(Item):
     """
     A class to represent an item of electronics traded at the swap meet.
     
@@ -15,9 +17,13 @@ class Electronics:
         Method to stringify Electronics. Returns default message.
     """
 
+    # TODO: Is there a less repetitive way to do this?
     def __init__(self, category="Electronics", condition=0):
         self.category = category
         self.condition = condition
+
+    # def __init__(self, category="Electronics", condition=0)
+    #     super().__init__(category="Electronics", condition=0)
 
     def __str__(self):
         """Method to stringify Electronics. Returns default message."""
