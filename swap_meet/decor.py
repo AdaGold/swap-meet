b0167e8f3433887360cabe34bf93b67e415a00c1
Lia Gaetano <lgaetano19@gmail.com>
2021-09-28 20:08:08 -0700

    Decor: add age attribute

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -11,13 +11,19 @@ class Decor(Item):
     category : str
         categorical descriptor for decor
 
+    condition : int
+        condition rating for item, values 1 (poor) to 5 (like new)
+    
+    age : int
+        age of item in years
+
     Methods
     ------- 
     __str__():
         Method to stringify Decor. Returns default message.
     """
-    def __init__(self, condition=0):
-        super().__init__(category="Decor", condition=condition)
+    def __init__(self, condition=0, age=0):
+        super().__init__(category="Decor", condition=condition, age=age)
 
     def __str__(self):
         """Method to stringify Decor. Returns default message."""
