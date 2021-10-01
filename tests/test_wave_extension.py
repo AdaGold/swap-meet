c2f0564a062fdfff93711d12d18947c71022181e
Lia Gaetano <lgaetano19@gmail.com>
2021-09-30 17:05:09 -0700

    Added test_swap_by_newest_when_all_items_have_ages() to test_wave_extension.py

--- tests/test_wave_extension.py
+++ tests/test_wave_extension.py
@@ -4,7 +4,39 @@ from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 
-def test_swap_by_newest():
+def test_swap_by_newest_when_all_items_have_ages():
+    item_a = Decor(condition=2.0, age=10)
+    item_b = Electronics(condition=4.0, age=5)
+    item_c = Decor(condition=4.0, age=45)
+    arie = Vendor(
+        inventory=[item_c, item_b, item_a]
+    )
+
+    item_d = Clothing(condition=2.0, age=3)
+    item_e = Decor(condition=4.0, age=45)
+    item_f = Clothing(condition=4.0, age=1)
+    jessica = Vendor(
+        inventory=[item_f, item_e, item_d]
+    )
+
+    result = arie.swap_by_newest(
+        other=jessica
+    )
+
+    assert not result
+    assert len(arie.inventory) == 3
+    assert len(jessica.inventory) == 3
+    assert item_a in arie.inventory
+    assert item_b not in arie.inventory
+    assert item_c in arie.inventory
+    assert item_f in arie.inventory
+    assert item_d in jessica.inventory
+    assert item_b in jessica.inventory
+    assert item_e in jessica.inventory
+    assert item_f not in jessica.inventory
+
+
+def test_swap_by_newest_when_ages_are_default_None():
     item_a = Decor(condition=2.0, age=10)
     item_b = Electronics(condition=4.0, age=5)
     item_c = Decor(condition=4.0, age=45)
