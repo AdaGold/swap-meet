069e454ae1348a2e54d014064528b3d84cc1452c
letypl12 <86847701+letypl12@users.noreply.github.com>
2021-10-21 07:15:14 -0700

    Update swap_meet/clothing.py
    Co-authored-by: jbieniosek <jbieniosek@gmail.com>

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, category = "Clothing", condition = 0):
+    def __init__(self, condition = 0):
+        category = "Clothing"
         super() .__init__(category=category, condition=condition)     
 
     def __str__(self):
