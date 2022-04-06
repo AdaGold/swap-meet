47dc35b52991a4dae8c5ec0bfade3f996a51c111
cathos <cathos@startmail.com>
2022-04-05 17:47:30 -0700

    finished waves 1 and 2, through test 2 in wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,39 @@
 class Vendor:
-    pass
+    def __init__ (self, inventory = None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self, item): 
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        try: 
+            self.inventory.remove(item)
+            return item
+        except:
+            return False
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+
+    def swap_items(self, vendor, my_item, their_item):
+        '''
+        add my item to vendor's inventory
+        remove my item from my inventory
+        add their item to my inventory
+        remove their item from their inventory
+        '''
+        try:
+            vendor.add(my_item)
+            self.remove(my_item)
+            self.add(their_item)
+            vendor.remove(their_item)
+            return True
+        except:
+            return False
