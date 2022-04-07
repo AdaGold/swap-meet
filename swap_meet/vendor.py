2accffdbb6033fd645afe50866471fe9e17d9d51
Raha <raha.mora1@gatech.edu>
2022-04-07 01:21:19 -0400

    vendor_all_methods_fixed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,85 @@
+import operator
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self,item):
+        result = self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError as ve:
+            return False
+
+    def get_by_category(self,category):
+        result = [item for item in self.inventory if item.category == category]
+        return result
+
+    def swap_items(self,other_vendor,my_item,their_item):
+        if not all ((other_vendor, my_item, their_item)):
+            return False
+        if  my_item == their_item:
+            return True
+        elif my_item in self.inventory and their_item in other_vendor.inventory:
+            self.add(their_item)
+            other_vendor.add(my_item)
+            other_vendor.remove(their_item)
+            self.remove(my_item)
+            return True
+
+    def swap_first_item(self, friend_vendor):
+        if not all((friend_vendor.inventory, self.inventory)):
+            return False 
+        
+        friend_vendor.add(self.inventory[0])
+        self.add(friend_vendor.inventory[0])
+        friend_vendor.remove(friend_vendor.inventory[0])
+        self.remove(self.inventory[0])
+        return True
+
+    def get_best_by_category(self,category):
+        matched_category = [item for item in self.inventory if category == item.category]
+        if len(matched_category) == 0:
+            return None
+        max = matched_category[0]
+        for item in matched_category:
+            if item.condition > max.condition:
+                max = item
+        return max
+
+    def swap_best_by_category(self,other,my_priority,their_priority):
+        their_choice = self.get_best_by_category(their_priority)
+        my_choice = other.get_best_by_category(my_priority)
+
+        if my_choice and their_choice:
+            self.swap_items(other, their_choice,  my_choice)
+            return True
+
+        return False
+
+    def newest_item(self):
+        if self.inventory == []:
+            return None
+        new_item = self.inventory[0]
+        for item in self.inventory:
+            if item.age < new_item.age:
+                new_item = item
+        return new_item
+
+        # sorted_inventory = self.inventory.sort(key = lambda item: item.age)
+        # return sorted_inventory
+
+    def swap_by_newest(self,other):
+        new_self = self.newest_item()
+        new_other = other.newest_item()
+        return self.swap_items(other, new_self,new_other)
+
+        # result = sorted_self.swap_first_item(sorted_other)
+        return result
+    # pass
