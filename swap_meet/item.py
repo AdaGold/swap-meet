f5578c5e91beffb01fb28c9fa6837bf3cf91a75a
Raha <raha.mora1@gatech.edu>
2022-04-07 01:20:53 -0400

    item_Class_passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,26 @@
 class Item:
-    pass
+    def __init__(self,category = "", condition = 0, age = 0):
+        self.category = category
+        self.condition = condition
+        self.age = age
+    def __str__(self):
+        
+        return f"Hello World!"
+    
+    def condition_description(self):
+        if 0 <= self.condition < 1:
+            return "Just trash it!"
+        elif 1 <= self.condition < 2:
+            return "Enjoy the last moments!"
+        elif 2 <= self.condition < 3:
+            return "As good as 30 year old wine!"
+        elif 3 <= self.condition < 4:
+            return "Better than the new one"
+        elif 4 <= self.condition <= 5:
+            return "No need to ask!"
+        else:
+            return False
+    
+        
+    
+    # pass
