e90cdf701adb0f09a3883932921002f4c4d14639
Raha <raha.mora1@gatech.edu>
2022-04-08 01:58:49 -0400

    all_doc_strings_added

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,30 @@
 class Item:
-    def __init__(self,category = "", condition = 0, age = 0):
+    """
+        A class to represent an item.
+
+    ...
+
+    Attributes
+    ----------
+    category : string (optional)
+        a string that represents the category of the item
+    condition:
+        a float number between 0 and 5 that represents the current condition of the item
+
+    age :
+        an integer that represents the age of the item
+
+    Methods
+    -------
+    condition_description:
+        - returns "Just trash it!" if condition is between 0 and 1
+        - returns "Enjoy the last moments!" if condition is between 1 and 2
+        - returns "As good as 30 year old wine!" if condition is between 2 and 3
+        - returns "Better than the new one" if condition is between 3 and 4
+        - returns "No need to ask!" if condition is between 4 and 5
+        - returns False if condition is not between 0 and 5
+    """
+    def __init__(self,category = "", condition = 0.0, age = 0):
         self.category = category
         self.condition = condition
         self.age = age
