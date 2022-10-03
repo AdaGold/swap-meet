bb647932267439dc70e870b3fd61ff158f134789
LP <lp.passarelli.wilson@gmail.com>
2022-10-03 11:55:31 -0700

    passed test 2 of wave 2 - get_by_category()

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,6 @@
 class Vendor:
     """The Vendor class has optional arguement inventory,
-    methods .add(item), .remove(item)"""
+    methods: .add(item), .remove(item), .get_by_category(category)"""
     def __init__(self, inventory = None):
         #if an inventory list is given, assign it to the attribute
         if inventory:
@@ -23,4 +23,12 @@ class Vendor:
         else:
             return False
 
-        
+    def get_by_category(self, category):
+        """given a category, return a list of all items in inventory
+        that have that as their attribute category."""
+        items_in_category = []
+        #iterate through inventory to append items with the given category
+        for item in self.inventory:
+            if item.category == category:
+                items_in_category.append(item)
+        return items_in_category
