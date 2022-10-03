a94fc8f0336ad6175ec9df83e18f3ebf63676b7f
LP <lp.passarelli.wilson@gmail.com>
2022-10-03 11:29:11 -0700

    passed the first 2 tests of wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,9 @@
 class Vendor:
     #testing a small change for git hub
-    pass
+    def __init__(self, inventory = None):
+        #if an inventory list is given, assign it to the attribute
+        if inventory:
+            self.inventory = inventory
+        #if no inventory list is given, initialize an empty list for inventory.
+        else:
+            self.inventory = []
