0d6d6bc8a009619f289b0ea8d48b7b35e3266b3e
Soleil <soleilherring@gmail.com>
2022-10-04 12:08:05 -0700

    Added functions to wave 3, 4, and 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,5 +2,24 @@
 
 class Item:
     
-    def __init__(self, category = None):
+    def __init__(self, category = None, condition = 0):
         self.category = category if category is not None else ""
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        self.condition = round(self.condition)
+        if self.condition == 0:
+            return "garbage"
+        elif self.condition == 1:
+            return "bad"
+        elif self.condition == 2:
+            return "used"
+        elif self.condition == 3:
+            return "lightly used"
+        elif self.condition == 4:
+            return "like new"
+        else:
+            return "new"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,3 +21,24 @@ class Vendor:
                 list_of_items.append(item)
         return list_of_items
 
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            self.remove(my_item)
+            other_vendor.add(my_item)
+        return True
+
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory: 
+            return False
+        else:
+            other_vendor.add(self.inventory[0])
+            self.remove(self.inventory[0])
+            self.add(other_vendor.inventory[0])
+            other_vendor.remove(other_vendor.inventory[0])
+        return True
+
+    
