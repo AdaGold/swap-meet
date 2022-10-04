3adeec7fcf179146a884266b0a00bb79118d5955
Kate <varindakate@gmail.com>
2022-10-03 23:30:00 -0700

    passed 4/5 tests WAVE05

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,9 @@
+from swap_meet.item import Item
+
 class Clothing:
-    pass
+    def __init__(self, category = "Clothing", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,6 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
-    
+
     def ___str___(self):
         return f"{Item}"
