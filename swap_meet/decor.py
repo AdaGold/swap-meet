b87351c782562c78db9721c2ce21e1ddc2067455
LP <lp.passarelli.wilson@gmail.com>
2022-10-04 10:31:01 -0700

    refactored wave 5 to inherit from item class. one more test to go in wave 5 still

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,8 @@
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     def __init__(self, category = "Decor", condition = 0.0):
-        self.category = category
-        self.condition = condition
+        super().__init__(category, condition)
 
     def __str__(self):
         return "Something to decorate your space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,5 @@
 class Item:
-    def __init__(self, category = "", conditon = 0.0):
+    def __init__(self, category = "", condition = 0.0):
         self.category = category
         self.condition = condition
     def __str__(self):
