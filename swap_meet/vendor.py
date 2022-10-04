878d3c318f1f6feef36e3f69a6a18958e570fbd6
LexElena22 <103533828+LexElena22@users.noreply.github.com>
2022-10-04 08:59:04 -0700

    completed wave 01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,23 @@
+from swap_meet.item import Item
+
+
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+
+    def add(self, added_item):
+        
+        self.inventory.append(added_item)
+
+        return added_item
+
+
+    def remove(self, removed_item):
+        
+        if removed_item in self.inventory:
+            self.inventory.remove(removed_item)
+        else:
+            return False
+        
+        return removed_item
