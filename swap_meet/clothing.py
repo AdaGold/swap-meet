618a62925e49164afeb0f5e6f6b5a8cc3a3d3913
LP <lp.passarelli.wilson@gmail.com>
2022-10-04 10:56:22 -0700

    passed all tests of wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,6 +2,7 @@ from swap_meet.item import Item
 
 class Clothing(Item):
     def __init__(self, category = "Clothing", condition = 0.0):
+        #is there a way to NOT have condition here??I don't think so. 
         super().__init__(category, condition)
     def __str__(self):
         return "The finest clothing you could wear."
