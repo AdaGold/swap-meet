2d63659be24819a1112b3dc91be9cd505904075d
LP <lp.passarelli.wilson@gmail.com>
2022-10-04 10:55:49 -0700

    passed all tests of wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,3 +4,14 @@ class Item:
         self.condition = condition
     def __str__(self):
         return "Hello World!"
+    def condition_description(self):
+        if self.condition <= 1:
+            return "OMG Don't swap for this!"
+        elif self.condition <= 2:
+            return "This item is well-loved"
+        elif self.condition <= 3:
+            return "This is average qualtiy"
+        elif self.condition <= 4:
+            return "This is in pretty good condition"
+        else:
+            return "Love this.  Great condition."
