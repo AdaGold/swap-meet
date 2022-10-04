36d55636241db516c107c1b8f6b588494fcabf94
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-10-04 16:50:22 -0700

    Forgot to commit until now. Passed Wave 1 and 2. Passed 11th test of Wave 3.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,9 @@
 class Item:
-    pass
+    
+    def __init__(self, category = None):
+        if category is None:
+            category = ""
+        self.category = category 
+    
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,41 @@
+from operator import itemgetter
+
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category):
+        items_list = []
+
+        for item in self.inventory:
+            if item.category == category: 
+                items_list.append(item)
+        
+        if items_list == []:
+            return "Sorry, there are no items within that category."
+
+        return items_list
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        
+        self.add(their_item)
+        self.remove(my_item)
+        vendor.add(my_item)
+        vendor.remove(their_item)
+        return True
