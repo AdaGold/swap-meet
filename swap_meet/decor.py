8e1f295099313702680c435b50e9a5a93a6d74d2
Nicole Mejia <snicolemejia@gmail.com>
2022-10-05 11:16:32 -0400

    Pass test items have condition as float

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,7 +2,8 @@ from .item import Item
 
 class Decor(Item):
     def __init__(self, category=None, condition=0.0):
-        super().__init__(category="Decor")
+        super().__init__(category, condition)
+        self.category = "Decor"
     
     def __str__(self):
         return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,7 +2,8 @@ from .item import Item
 
 class Electronics(Item):
     def __init__(self, category=None, condition=0.0):
-        super().__init__(category="Electronics")
+        super().__init__(category, condition)
+        self.category = "Electronics"
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
