cce24a04100036556ba94eb4d16532e721147ba1
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-10-06 07:41:32 -0700

    Passed test 23 in Wave 5. Implemented Item as a parent class for Clothing, Decor, and Electronic classes to use the condition_description instance method within Item.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,17 +9,17 @@ class Item:
     def __str__(self):
         return "Hello World!"
 
-    def condition_description(self, num):
-        if num >= 5:
+    def condition_description(self):
+        if self.condition >= 5:
             return "Perfect in every way!"
-        elif num >= 4:
+        elif self.condition >= 4:
             return "Not bad, not bad"
-        elif num >= 3:
+        elif self.condition >= 3:
             return "I've seen worse"
-        elif num >= 2:
+        elif self.condition >= 2:
             return "Well, at least it's free"
-        elif num >= 1:
+        elif self.condition >= 1:
             return "Better than a poke in the eye with a sharp stick"
-        elif num > 0:
+        elif self.condition > 0:
             return "Oof"
         
