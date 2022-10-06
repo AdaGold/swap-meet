3ab563f82d9b7242c806e69b3c61cd60a03820bb
Anaiq <>
2022-10-06 08:47:42 -0500

    completed get_best_by_category wave 6, updated item.py, unskipped tests  test_wave_06

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -51,26 +51,28 @@ class Vendor:
 
 # WAVE 6
     def get_best_by_category(self, category):
-        print(self.inventory)
-        # search self.inventory for list of items with specific category
         list_of_items = self.get_by_category(category)
-        print(list_of_items)
+        if not list_of_items:
+            return None
+        else:
+            best_item =  max(list_of_items, key = lambda item: item.condition)
         
-        # search list of items for highest condition
-        best_condition = list_of_items[0]
-        for item in list_of_items:
-            ...
-
-        # if item found, return item
-        # if item not found,  return None
+        return best_item
 
 
-#     def swap_best_by_category(self, other, my_priority, their_priority):#swaps the best item of certain categories with other_vendor
-#         """
-#         other, which represents another Vendor instance to trade with
-#         my_priority, which represents a category that the Vendor wants to receive
-#         their_priority, which represents a category that other wants to receive
-#         """
+    def swap_best_by_category(self, other, my_priority, their_priority):#swaps the best item of certain categories with other_vendor
+        
+        # if their_priority not in self.inventory or my_priority not in other.inventory:
+        #     return False
+        
+        best_item = max(self.inventory, key = lambda item: item.condition)
+        print(f"best item: {best_item}")
+        # self.inventory.remove(my_item)
+        # vendor_friend.inventory.append(my_item)
+
+        # vendor_friend.inventory.remove(their_item)
+        # self.inventory.append(their_item)
+        # return True
         
 #         # best item in self.inventory that matches their_priority is SWAPPED with the best item in other_vendor inventory that matches my_priority
 #         # 1.my_priority
@@ -81,6 +83,18 @@ class Vendor:
 #         #     4.b.  if no match:returns False
 #         # 5. swap the best items
 #             # : can we call  swap_items(): here so that swap_best_by_category and swap_first_item can use it???
-
-#         # return True
+        
 #         ...
+
+    # # helper_function
+    # def get_best_item(self, list_of_items):
+    #     if not list_of_items:
+    #         return None
+        
+    #     best_item = list_of_items[0]
+
+    #     for item in list_of_items:
+    #         if item.condition > best_item.condition:
+    #             best_item = item
+
+    #         return best_item
--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -20,7 +20,7 @@ def test_best_by_category():
     assert best_item.category == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_best_by_category_no_matches_is_none():
     item_a = Decor(condition=2.0)
     item_b = Decor(condition=2.0)
@@ -33,7 +33,7 @@ def test_best_by_category_no_matches_is_none():
 
     assert best_item is None
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_best_by_category_with_duplicates():
     # Arrange
     item_a = Clothing(condition=2.0)
@@ -50,7 +50,7 @@ def test_best_by_category_with_duplicates():
     assert best_item.category == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category():
     # Arrange
     # me
@@ -76,14 +76,25 @@ def test_swap_best_by_category():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
     # Assertions should check:
     # - That the results is truthy
+    assert result
     # - That tai and jesse's inventories are the correct length
+    assert len(tai.inventory) == 3
+    assert len(tai.inventory) == 3
+
     # - That all the correct items are in tai and jesse's inventories, including the items which were swapped from one vendor to the other
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_f in tai.inventory
+    assert item_e in jesse.inventory
+    assert item_d in jesse.inventory
+    assert item_c in jesse.inventory
+    
 
 @pytest.mark.skip
 def test_swap_best_by_category_reordered():
