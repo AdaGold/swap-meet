0496e0f90c3c20906ef949418e1de11193ad019c
LexElena22 <103533828+LexElena22@users.noreply.github.com>
2022-10-06 19:59:41 -0700

    Completed Wave 02

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,22 +2,32 @@ from swap_meet.item import Item
 
 
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
         self.inventory = inventory
 
 
-    def add(self, added_item):
+    def add(self, item):
         
-        self.inventory.append(added_item)
+        self.inventory.append(item)
 
-        return added_item
+        return item
 
 
-    def remove(self, removed_item):
+    def remove(self, item):
         
-        if removed_item in self.inventory:
-            self.inventory.remove(removed_item)
+        if item in self.inventory:
+            self.inventory.remove(item)
         else:
             return False
         
-        return removed_item
+        return item
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+
