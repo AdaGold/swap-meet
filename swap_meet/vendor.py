c0a71f0e2db941ddfdff1d7569ae8114335d4386
liz <lissete.trejo@gmail.com>
2023-04-03 15:01:51 -0700

    wave 1 - add __init__, add(), remove() methods to Vendor class. wave 2 - add __init__, get_category() methods to Item class, add get_by_id() method to Vendor class. complete asertions for test_removing_not_found_if_false. ensure all tests related to wave 1 and wave 2 pass.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,26 @@
+#Wave 1
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None: 
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item 
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else: 
+            return False
+        
+#Wave2
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
+    
