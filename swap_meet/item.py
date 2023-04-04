aead876ed82178d1074c13f5637704def4bf9c46
Sel <selenetenorio27@hotmail.com>
2023-04-03 22:33:27 -0700

    Implements wave_03 and passing unit_tests wave_03

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,3 +9,9 @@ class Item:
 
     def get_category(self):
         return Item.__name__
+    
+    def get_category(self, item):
+        return item.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
