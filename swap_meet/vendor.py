8aa064c54d32914a6948923495841c280d5b1a8d
Linh Huynh <greenlinh@gmail.com>
2023-04-05 12:04:00 -0700

    Refactored my_item instance method in Item class.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,15 +25,24 @@ class Vendor:
         return None
     
     def swap_items(self, other_vendor, my_item, their_item):
-        # other_vendor = the friend the vendor is swapping with
-        # my_item = the item this Vendor instance plans to give
-        # their_item = item the friend Vendor plans to give
+    #     # other_vendor = the friend the vendor is swapping with
+    #     # my_item = the item this Vendor instance plans to give
+    #     # their_item = item the friend Vendor plans to give
         
-        if my_item in self.inventory or their_item in other_vendor.inventory:
-            self.inventory.append(their_item)           
+    #     if my_item not in self.inventory or their_item not in other_vendor.inventory:
+    #         return False
+    #     else:
+    #         self.inventory.remove(my_item)
+    #         self.inventory.append(their_item)
+    #         other_vendor.inventory.remove(their_item)
+    #         other_vendor.inventory.append(my_item)
+    #         return True
+
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
             other_vendor.inventory.remove(their_item)
             other_vendor.inventory.append(my_item)
-            self.inventory.remove(my_item)
             return True
-        else:
-            return False
+        return False
+    
