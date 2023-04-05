ba47b837dad61c4762d545e6cd6a2e5239cdb582
Linh Huynh <greenlinh@gmail.com>
2023-04-05 04:00:26 -0700

    Added __str__ instance method in the item.py module to return the string representation of the item type and id.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -11,4 +11,7 @@ class Item:
 
     def get_category(self):
         return type(self).__name__
+        #return self.__class__.__name__
 
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
