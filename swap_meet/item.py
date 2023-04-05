8cd27142195e5b0feb2e822ce7f921a0263233ce
Linh Huynh <greenlinh@gmail.com>
2023-04-05 02:57:29 -0700

    Created Item class and constructor with id attribute and method instance named get_category.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
+import uuid
+
+# unique_id = int(uuid.uuid4())
+# print(unique_id)
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        if id is None:
+            id = int(uuid.uuid4())
+        self.id = id
+
+    def get_category(self):
+        return type(self).__name__
+
