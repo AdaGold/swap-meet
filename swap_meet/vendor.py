77024f5fb17d551c532d5c5b165fc1720d6b857e
Linh Huynh <greenlinh@gmail.com>
2023-04-07 07:12:17 -0700

    Wrote get_by_category instance method within the Vendor class of the vendor.py module.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,28 +24,7 @@ class Vendor:
                 return item
         return None
     
-    def swap_items(self, other_vendor, my_item, their_item):
-    #     # other_vendor = the friend the vendor is swapping with
-    #     # my_item = the item this Vendor instance plans to give
-    #     # their_item = item the friend Vendor plans to give
-        
-    #     if my_item not in self.inventory or their_item not in other_vendor.inventory:
-    #         return False
-    #     else:
-    #         self.inventory.remove(my_item)
-    #         self.inventory.append(their_item)
-    #         other_vendor.inventory.remove(their_item)
-    #         other_vendor.inventory.append(my_item)
-    #         return True
-
-        # if my_item in self.inventory and their_item in other_vendor.inventory:
-        #     self.inventory.remove(my_item)
-        #     self.inventory.append(their_item)
-        #     other_vendor.inventory.remove(their_item)
-        #     other_vendor.inventory.append(my_item)
-        #     return True
-        # return False
-    
+    def swap_items(self, other_vendor, my_item, their_item):    
         if my_item in self.inventory and their_item in other_vendor.inventory:
             self.remove(my_item)
             self.add(their_item)
@@ -65,3 +44,16 @@ class Vendor:
             self.add(their_first_item)
             other_vendor.remove(their_first_item)
             return True
+        
+    def get_by_category(self, category):
+        category_inventory = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                category_inventory.append(item)
+        return category_inventory
+
+    def get_best_by_category(self):
+    
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+    
+        
