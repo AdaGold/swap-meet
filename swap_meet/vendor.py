e4577e8f88de22fb73659dacc137698b9b2b32d6
Linh Huynh <greenlinh@gmail.com>
2023-04-07 07:21:24 -0700

    Wrote the get_best_by_category instance method for the Vendor class in the vendor.py module.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,10 +7,12 @@ class Vendor:
         else:
             self.inventory = inventory
 
+
     def add(self, item):
         self.inventory.append(item)
         return item
     
+
     def remove(self, item):
         if item not in self.inventory:
             return False
@@ -18,12 +20,14 @@ class Vendor:
             self.inventory.remove(item)
         return item
     
+
     def get_by_id(self, id):
         for item in self.inventory:
             if id == item.id:
                 return item
         return None
     
+
     def swap_items(self, other_vendor, my_item, their_item):    
         if my_item in self.inventory and their_item in other_vendor.inventory:
             self.remove(my_item)
@@ -33,6 +37,7 @@ class Vendor:
             return True
         return False    
     
+
     def swap_first_item(self, other_vendor):
         if not self.inventory or not other_vendor.inventory:
             return False
@@ -45,6 +50,7 @@ class Vendor:
             other_vendor.remove(their_first_item)
             return True
         
+
     def get_by_category(self, category):
         category_inventory = []
         for item in self.inventory:
@@ -52,8 +58,18 @@ class Vendor:
                 category_inventory.append(item)
         return category_inventory
 
-    def get_best_by_category(self):
+
+    def get_best_by_category(self, category):
+        category_selection = self.get_by_category(category)
+        if not category_selection:
+            return None
+        else:
+            highest_condition = category_selection[0]
+            for item in category_selection:
+                if item.condition > highest_condition.condition:
+                    highest_condition = item
+            return highest_condition
+
     
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
     
-        
